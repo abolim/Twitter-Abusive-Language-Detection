4122771ce7ac8e79ba76b18085964fb72a80e2b1
Mayank Goel <mickkygoel@gmail.com>
2020-02-06 17:45:10 -0800

    Adding notes folder for scrap code pieces. Updating .rmd file to include tweet classification

---
+++ notes/data_exploration.R
@@ -0,0 +1,9 @@
+# While the code works, we faced issues with properly formatting the output
+
+df = df[c("tweet_id", "tweet_text", "does_this_tweet_contain_hate_speech")]
+print(c("total number of tweets", dim(df)[1]))
+head(df)
+dim(df)
+str(df)
+summary(df)
+colnames(df)
