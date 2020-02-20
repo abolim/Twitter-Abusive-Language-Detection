cbb7913e1ce41a741b1aa59ad5d1efccbd687a27
aboli.moroney <aboli.moroney9@gmail.com>
2020-02-20 15:36:40 -0800

    Data cleaning - Added comments and removed redundant code

--- analysis/001-tweet-cleaning.R
+++ analysis/001-tweet-cleaning.R
@@ -1,15 +1,21 @@
 ## ---- Input-Data
+
 raw_data <- read.csv("Data/twitter-hate-speech-classifier-data.csv")
 raw_data <- raw_data[c('tweet_text', 'does_this_tweet_contain_hate_speech', 'tweet_id')]
+# Filtering first 3000 rows due to limited computational capacity
 raw_data <- raw_data[1:3000,]
+# Converting classification to numeric values
 raw_data$class <- as.numeric(raw_data$does_this_tweet_contain_hate_speech)
-unique(raw_data$class)
-toString(raw_data$tweet_text) 
+unique(raw_data$class) #checking values
+toString(raw_data$tweet_text) #converting tweets to text class
 
-library(stringr) #for data prep
-library(tm) #stop word removal
 
-## @knitr Loading-Libraries
+## ---- Loading-Libraries
+
+library(stringr)  #for data prep
+library(tm)       #stop word removal
+library(quanteda) #tokenization
+
 ## ---- Data-Preparation
 
 # Remove everything that is not a number or letter
@@ -28,9 +34,6 @@ raw_data$tweet_clean1  = removeWords(raw_data$tweet_clean1,stopwords)
 # Shrink down to just one white space
 raw_data$tweet_clean1 <- stringr::str_replace_all(raw_data$tweet_clean1,"[\\s]+", " ")
 
-## @knitr Tokenization
-library(quanteda) #tokenization
-
 # One hot encoding of tokens
 datadb <- data.frame (
   Class = raw_data$class,
@@ -42,6 +45,8 @@ dtm <- DocumentTermMatrix(corpus)
 dtm2 <-  cbind(datadb$Class, as.matrix(dtm))
 colnames(dtm2) <- c("Classification", colnames(dtm))
 # dtm2[1:5, 1:5] #Quality check
+
+## ---- Output-Data
 
 #Writing document-term-matrix into csv file
 write.csv(dtm2, "prepared_tweet_dataset.csv")
