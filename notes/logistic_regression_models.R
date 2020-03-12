36d46d5bbae73e80e1738a3e4f141f371754aee3
Harini Ram Prasad <hramprasad98@gmail.com>
2020-03-12 15:19:39 -0700

    Description file added

--- notes/logistic_regression_models.R
+++ notes/logistic_regression_models.R
@@ -9,3 +9,4 @@ model_randomF <- randomForest(Classification ~ ., data = test, importance = TRUE
 p=predict(model_randomF,test_x)
 precision=posPredValue(factor(p),factor(test_y),positive=“1”)
 recall=sensitivity(p,test_y,positive=“1”)
+
