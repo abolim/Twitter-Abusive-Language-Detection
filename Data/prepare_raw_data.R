dd6728be680224bbed796c07f8bc283ec98126f6
Ben Marwick <benmarwick@hotmail.com>
2020-03-14 01:06:39 -0700

    Update prepare_raw_data.R

--- Data/prepare_raw_data.R
+++ Data/prepare_raw_data.R
@@ -1,13 +1,13 @@
 
 # just do this once to remove the remove 'Warning: found non-ASCII strings'
 
-raw_data <- read.csv(here("Data","twitter-hate-speech-classifier-data.csv"))
+# raw_data <- read.csv(here("Data","twitter-hate-speech-classifier-data.csv"))
 
-Encoding(levels(raw_data$tweet_text )) <- "latin1"
-levels(raw_data$tweet_text) <- iconv(
-  levels(raw_data$tweet_text), 
-  "latin1", 
-  "UTF-8"
-)
+# Encoding(levels(raw_data$tweet_text )) <- "latin1"
+# levels(raw_data$tweet_text) <- iconv(
+#  levels(raw_data$tweet_text), 
+#  "latin1", 
+#  "UTF-8"
+# )
 
-write.csv(raw_data, here("Data","twitter-hate-speech-classifier-data.csv"))
+# write.csv(raw_data, here("Data","twitter-hate-speech-classifier-data.csv"))
